39cbb24b729bffa7dfe11660d63f85405370891c
leticea <nascimento.leticeah@gmail.com>
2022-12-26 16:34:14 -0300

    criando as variáveis

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 let canvas = document.getElementById("canvas");
 let context = canvas.getContext("2d");
 
-let angleIncrement = (30 * Math.PI) / 180
-let startX = canvas.width / 2
-let startY = canvas.height / 2
-let height = (canvas.height * 7) / 24
+let angleIncrement = (30 * Math.PI) / 180;
+let startX = canvas.width / 2;
+let startY = canvas.height / 2;
+let height = (canvas.height * 7) / 24;
 
-let thickness = 5
-let maxDepth = 8
-let count = 0
-let branchPropagation = 5
+let thickness = 5;
+let maxDepth = 8;
+let count = 0;
+let branchPropagation = 5;
+
+
+
+let drawBranch = (x, y, height, thickness, angle, depth) => {
+  if (depth > maxDepth) return;
+  let endX = x - height * Math.sin(angle);
+  let endY = y - height * Math.cos(angle);
+
+  drawLine(x, y, endX, endY, thickness, "black");
+};
+
